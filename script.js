e86492d06dcdf71fd925722828368946481a9fa6
Pedro Suns <pedrobattasiniemp@gmail.com>
2025-04-29 12:47:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById('botao-benef');
+    
+    if (botao) {
+        botao.addEventListener('click', () => {
+            window.location.href = '';
+        });
+    }
+});
